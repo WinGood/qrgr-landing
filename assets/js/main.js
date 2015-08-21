d5b6db9f33ecea26331b4b725129685a936fc08f
zzzaki <techokill@gmail.com>
2015-08-21 21:10:15 +0700

    js формы

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,20 @@
-var jQuery;
-
-jQuery = require('jquery');
-
-window['jQuery'] = window['$'] = jQuery;
-
-$('body, html').click(function() {
-  return console.log('test');
+$(document).ready(function() {
+  $('.scroll-to-form').bind('click', function() {
+    $('html, body').stop().animate({
+      scrollTop: $('#section-8').offset().top
+    }, 1000);
+    return false;
+  });
+  return $('.modal-btn').fancybox({
+    width: 380,
+    autoHeight: true,
+    autoSize: false,
+    padding: 0,
+    scrolling: 'no',
+    helpers: {
+      overlay: {
+        locked: false
+      }
+    }
+  });
 });
--- src/coffee/main.coffee
+++ src/coffee/main.coffee
@@ -1,5 +1,21 @@
-jQuery = require('jquery')
-window['jQuery'] = window['$'] = jQuery;
+#jQuery = require('jquery')
+#window['jQuery'] = window['$'] = jQuery;
 
-$('body, html').click ->
-  console.log('test')
+$(document).ready ->
+  $('.scroll-to-form').bind 'click', ->
+    $('html, body').stop().animate({
+      scrollTop: $('#section-8').offset().top
+    }, 1000)
+    return false
+  $('.modal-btn').fancybox({
+    width: 380,
+    autoHeight: true,
+    autoSize: false,
+    padding: 0,
+    scrolling: 'no',
+    helpers: {
+      overlay: {
+        locked: false
+      }
+    }
+  })
